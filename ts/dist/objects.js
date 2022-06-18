e527fdec0530371695ab3bf3e8a9c4f38681a609
zishan <mactyller99@gmail.com>
2022-06-18 20:22:29 +0530

    m

--- ts/dist/objects.js
+++ ts/dist/objects.js
@@ -8,3 +8,7 @@ let emp = {
 // cannot assign key value pairs of which keys are not defined
 // emp.id=789;
 // cannot assign id value if readonly variable is declared
+let emp1 = {
+    id: 89
+};
+// already defined key  types in emp_type no need to define again
--- ts/src/objects.ts
+++ ts/src/objects.ts
@@ -1,3 +1,6 @@
+
+type emp_type={readonly id:number,name?:string}
+
 let emp:{readonly id:number,name?:string}={
     id:10,
     //here name is optional
@@ -8,3 +11,10 @@ let emp:{readonly id:number,name?:string}={
 // cannot assign key value pairs of which keys are not defined
 // emp.id=789;
 // cannot assign id value if readonly variable is declared
+
+let emp1:emp_type={
+    id:89
+};
+
+
+// already defined key  types in emp_type no need to define again
