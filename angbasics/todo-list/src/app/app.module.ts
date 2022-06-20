d537efabfca22a9c2029a9893af38ce17a7e8e60
Biren Yadav <byadav@masaglobal.com>
2022-06-20 13:55:00 +0530

    m

--- angbasics/todo-list/src/app/app.module.ts
+++ angbasics/todo-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './Mycomponents/todos/todos.component';
+import { TodoItemComponent } from './Mycomponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
