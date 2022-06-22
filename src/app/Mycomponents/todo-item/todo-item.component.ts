d4e77675c53db1e7bd041e5c284c8f45b24be829
Biren Yadav <byadav@masaglobal.com>
2022-06-22 13:11:11 +0530

    m

---
+++ src/app/Mycomponents/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo:Todo|undefined;
+  @Output() todoDelete: EventEmitter<Todo>=new EventEmitter();
+  constructor() { }
+
+  onClick(todo:Todo|undefined){
+    this.todoDelete.emit(todo);
+    console.log('click has triggered');
+    }
+
+  ngOnInit(): void {
+  }
+
+
+  
+}
